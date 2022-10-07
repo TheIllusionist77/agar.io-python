1950279b1e75455accc6a7ed844477f87bb4da25
TheIllusionist77 <113849310+TheIllusionist77@users.noreply.github.com>
2022-10-06 22:55:23 -0400

    Update agar.io-singleplayer.py
    Made the bots smarter, and added mass decay.

--- agar.io-singleplayer.py
+++ agar.io-singleplayer.py
@@ -18,11 +18,13 @@ bots_min_size = 25
 bots_max_size = 250
 respawn_cells = True
 respawn_bots = False
+bot_pursuit_range = 500
 player_color = (255, 0, 0)
 background_color = (0, 0, 0)
 text_color = (255, 255, 255)
 
 # SYSTEM VARIABLES
+TINYFONT = pygame.font.Font("freesansbold.ttf", 16)
 FONT = pygame.font.Font("freesansbold.ttf", 32)
 BIGFONT = pygame.font.Font("freesansbold.ttf", 72)
 WIDTH = 1280
@@ -41,36 +43,128 @@ class Cell():
         self.name = name
         self.radius = radius
         self.color = color
-        self.status = random.randint(1, 8)
         self.x_pos = x
         self.y_pos = y
+        if self.name == "Bot":
+            self.wandering = True
+            self.pursuit = False
+            self.pursuiting = "None"
+            self.running = False
+            self.chaser = "None"
     
-    # MAKES THE BOTS WANDER
-    def wander(self):
-        randomize = random.randint(1, round(self.radius))
-        if randomize == 1:
-            self.status = random.randint(1, 8)
-        
-        if self.status == 1:
-            self.y_pos += 300 / self.radius
-        elif self.status == 2:
-            self.x_pos += 150 / self.radius
-            self.y_pos += 150 / self.radius
-        elif self.status == 3:
-            self.x_pos += 300 / self.radius
-        elif self.status == 4:
-            self.x_pos += 150 / self.radius
-            self.y_pos -= 150 / self.radius
-        elif self.status == 5:
-            self.y_pos -= 300 / self.radius
-        elif self.status == 6:
-            self.x_pos -= 150 / self.radius
-            self.y_pos -= 150 / self.radius
-        elif self.status == 7:
-            self.x_pos -= 300 / self.radius
-        elif self.status == 8:
-            self.x_pos -= 150 / self.radius
-            self.y_pos += 150 / self.radius
+    # MAKES THE BOTS INTELLIGENT
+    def intelligence(self):
+        global cells, bots, bot_pursuit_range
+        if self.wandering == True:
+            smallest_distance = 4000
+            for cell in cells:
+                distance = math.sqrt(((cell.x_pos - self.x_pos) ** 2) + ((cell.y_pos - self.y_pos) ** 2))
+                if distance <= smallest_distance:
+                    smallest_distance = distance
+                    closest_cell = cell
+            
+            smallest_bot_distance = 4000
+            for bot in bots:
+                distance = math.sqrt(((bot.x_pos - self.x_pos) ** 2) + ((bot.y_pos - self.y_pos) ** 2))
+                if distance <= smallest_bot_distance and bot != self:
+                    smallest_bot_distance = distance
+                    closest_bot = bot
+            
+            if smallest_bot_distance <= self.radius + bot_pursuit_range * 0.75:
+                if closest_bot.radius * 1.1 <= self.radius:
+                    self.wandering = False
+                    self.pursuit = True
+                    self.pursuiting = closest_bot
+                else:
+                    self.running = True
+                    self.chaser = closest_cell
+            
+            if closest_cell.x_pos < self.x_pos:
+                self.x_pos -= 150 / self.radius
+            else:
+                self.x_pos += 150 / self.radius
+                
+            if closest_cell.y_pos < self.y_pos:
+                self.y_pos -= 150 / self.radius
+            else:
+                self.y_pos += 150 / self.radius
+            
+            player_distance = math.sqrt(((player_cell.x_pos - (WIDTH / 2) + self.x_pos) ** 2) + ((player_cell.y_pos - (HEIGHT / 2) + self.y_pos) ** 2))
+            if player_distance <= self.radius + bot_pursuit_range * 0.75:
+                if player_cell.radius * 1.1 < self.radius:
+                    self.wandering = False
+                    self.pursuit = True
+                    self.pursuiting = "Player"
+                else:
+                    self.wandering = False
+                    self.running = True
+                    self.chaser = "Player"
+        elif self.pursuit == True:
+            if self.pursuiting == "Player":
+                if player_cell.x_pos < -(self.x_pos - (WIDTH / 2)):
+                    self.x_pos += 150 / self.radius
+                else:
+                    self.x_pos -= 150 / self.radius
+                    
+                if player_cell.y_pos < -(self.y_pos - (HEIGHT / 2)):
+                    self.y_pos += 150 / self.radius
+                else:
+                    self.y_pos -= 150 / self.radius
+                
+                player_distance = math.sqrt(((player_cell.x_pos - (WIDTH / 2) + self.x_pos) ** 2) + ((player_cell.y_pos - (HEIGHT / 2) + self.y_pos) ** 2))
+                if player_distance >= self.radius + bot_pursuit_range or player_cell.radius * 1.1 >= self.radius:
+                    self.wandering = True
+                    self.pursuit = False
+                    self.pursuiting = "None"
+            elif self.pursuiting != "None":
+                if self.pursuiting.x_pos < self.x_pos:
+                    self.x_pos -= 150 / self.radius
+                else:
+                    self.x_pos += 150 / self.radius
+                    
+                if self.pursuiting.y_pos < self.y_pos:
+                    self.y_pos -= 150 / self.radius
+                else:
+                    self.y_pos += 150 / self.radius
+                  
+                bot_distance = math.sqrt(((self.pursuiting.x_pos - self.x_pos) ** 2) + ((self.pursuiting.y_pos - self.y_pos) ** 2))
+                if bot_distance >= self.radius + bot_pursuit_range or self.pursuiting.radius * 1.1 >= self.radius or len(bots) == 1 or self.pursuiting not in bots:
+                    self.wandering = True
+                    self.pursuit = False
+                    self.pursuiting = "None"
+        elif self.running == True:
+            if self.chaser == "Player":
+                if player_cell.x_pos < -(self.x_pos - (WIDTH / 2)):
+                    self.x_pos -= 150 / self.radius
+                else:
+                    self.x_pos += 150 / self.radius
+                    
+                if player_cell.y_pos < -(self.y_pos - (HEIGHT / 2)):
+                    self.y_pos -= 150 / self.radius
+                else:
+                    self.y_pos += 150 / self.radius
+                
+                player_distance = math.sqrt(((player_cell.x_pos - (WIDTH / 2) + self.x_pos) ** 2) + ((player_cell.y_pos - (HEIGHT / 2) + self.y_pos) ** 2))
+                if player_distance >= self.radius + bot_pursuit_range * 1.1 or player_cell.radius * 1.1 <= self.radius:
+                    self.wandering = True
+                    self.running = False
+                    self.chaser = "None"
+            elif self.chaser != "None":
+                if self.chaser.x_pos < self.x_pos:
+                    self.x_pos += 150 / self.radius
+                else:
+                    self.x_pos -= 150 / self.radius
+                    
+                if self.chaser.y_pos < self.y_pos:
+                    self.y_pos += 150 / self.radius
+                else:
+                    self.y_pos -= 150 / self.radius
+                    
+                bot_distance = math.sqrt(((self.chaser.x_pos - (WIDTH / 2) + self.x_pos) ** 2) + ((self.chaser.y_pos - (HEIGHT / 2) + self.y_pos) ** 2))
+                if bot_distance >= self.radius + bot_pursuit_range * 1.1 or self.chaser.radius * 1.1 <= self.radius:
+                    self.wandering = True
+                    self.running = False
+                    self.chaser = "None"
     
     # CHECKS IF AN CELL IS COLLIDING WITH SOMETHING
     def collide_check(self, player):
@@ -91,7 +185,7 @@ class Cell():
                 player.radius = new_radius
                 bots.remove(bot)
                 if respawn_bots:
-                    new_bot = Cell(random.randint(-map_size, map_size), random.randint(-map_size, map_size), (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), random.randint(25, 250), "Bot")
+                    new_bot = Cell(random.randint(-map_size, map_size), random.randint(-map_size, map_size), (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), random.randint(bots_min_size, bots_max_size), "Bot")
                     bots.append(new_bot)
             elif math.sqrt((player.x_pos - (WIDTH / 2) + bot.x_pos) ** 2 + (player.y_pos - (HEIGHT / 2) + bot.y_pos) ** 2) <= bot.radius and bot.radius >= player.radius * 1.1 and not game_over:
                 bot_area = math.pi * (bot.radius ** 2)
@@ -110,7 +204,7 @@ class Cell():
                         new_radius = math.sqrt(new_area / math.pi)
                         bot.radius = new_radius
                         if respawn_bots:
-                            new_bot = Cell(random.randint(-map_size, map_size), random.randint(-map_size, map_size), (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), random.randint(25, 250), "Bot")
+                            new_bot = Cell(random.randint(-map_size, map_size), random.randint(-map_size, map_size), (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), random.randint(bots_min_size, bots_max_size), "Bot")
                             bots.append(new_bot)
                 for cell in cells:
                     if math.sqrt((bot.x_pos - cell.x_pos) ** 2 + (bot.y_pos - cell.y_pos) ** 2) <= cell.radius + bot.radius and cell.radius <= bot.radius:
@@ -126,6 +220,18 @@ class Cell():
         if self.name == "Bot" or self.name == "Player":
             text = FONT.render(str(round(self.radius)), False, text_color)
             SCREEN.blit(text, (x - 17.5, y - 12.5))
+            if self.name == "Bot" and self.wandering == True:
+                text = TINYFONT.render("Wandering", False, text_color)
+                SCREEN.blit(text, (x - 45, y - 30))
+            elif self.name == "Bot" and self.pursuit == True:
+                text = TINYFONT.render("In Pursuit", False, text_color)
+                SCREEN.blit(text, (x - 40, y - 30))
+            elif self.name == "Bot" and self.running == True:
+                text = TINYFONT.render("Running Away", False, text_color)
+                SCREEN.blit(text, (x - 52.5, y - 30))
+            else:
+                text = TINYFONT.render("Player", False, text_color)
+                SCREEN.blit(text, (x - 17.5, y - 30))
 
 # SPAWNS THE ORIGINAL CELLS, BOTS AND PLAYER
 for i in range(cell_count):
@@ -135,7 +241,8 @@ for i in range(cell_count):
 for i in range(bot_count):
     new_bot = Cell(random.randint(-map_size, map_size), random.randint(-map_size, map_size), (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), random.randint(bots_min_size, bots_max_size), "Bot")
     bots.append(new_bot)
-    
+
+global player_cell
 player_cell = Cell(0, 0, player_color, spawn_size, "Player")
   
 # MAIN GAME LOOP
@@ -180,7 +287,9 @@ while True:
             bot.y_pos -= 5
         elif bot.y_pos <= -map_size:
             bot.y_pos += 5
-        bot.wander()
+        if bot.radius > 25:
+            bot.radius -= bot.radius / 25000
+        bot.intelligence()
         bot.draw(SCREEN, bot.x_pos + player_cell.x_pos, bot.y_pos + player_cell.y_pos)
     
     # CHECKING IF THE GAME IS OVER
@@ -190,7 +299,7 @@ while True:
     else:
         player_cell.draw(SCREEN, (WIDTH / 2), (HEIGHT / 2))
         if player_cell.radius > 25:
-            player_cell.radius -= 0.0025
+            player_cell.radius -= player_cell.radius / 25000
     
     # DRAWING THE MASS TO THE SCREEN
     text = FONT.render("Mass: " + str(round(player_cell.radius, 1)), False, text_color)
